fdde83524dc0e362cde43cb3c2e6d487541c0049
E - Coders (Aasheesh Agarwal) <83082760+e-coders@users.noreply.github.com>
2021-09-13 10:40:28 -0700

    Add files via upload
    Uploaded because edited offline...

---
+++ main.js
@@ -0,0 +1,7 @@
+const loader = document.querySelector("#loader");
+const content = document.querySelector("#main");
+
+function load(){
+	loader.style.display ='none';
+	content.style.display ='block';
+}
